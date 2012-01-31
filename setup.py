9ea0506523feb075248dd2850301253ade027ed2
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-01-31 12:59:57 +0100

    Putting libredis in package should help

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pylibredis',
-      py_modules=['pylibredis/libredis'],
+      packages=find_packages(),
       version='0.0.1' # Dummy version number for now.
      )
